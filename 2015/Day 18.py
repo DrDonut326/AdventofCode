cbe98ea9e7a1b2e99f696554bdeda1228baae609
Patrick Naven <pnaven@gmail.com>
2021-01-31 09:27:15 +0900

    Day 18. Implemented a helper Grid class to hopefully use with future grid problems.  Is pretty clunky but should be usable for a variety of applications.
    Might want to change how addresses are stored in the future.

---
+++ 2015/Day 18.py
@@ -0,0 +1,88 @@
+from Grids import DictGrid
+from GetInput import get_input
+from copy import deepcopy
+
+
+def set_corners(grid):
+    s = grid.size - 1
+    grid.add_data_to_grid_at_x_y_z('#', 0, 0)
+    grid.add_data_to_grid_at_x_y_z('#', 0, s)
+    grid.add_data_to_grid_at_x_y_z('#', s, 0)
+    grid.add_data_to_grid_at_x_y_z('#', s, s)
+
+
+def init_grid(grid, part=1):
+    starting_position = get_input('line')
+    # y
+    for y, line in enumerate(starting_position):
+        # x
+        for x, letter in enumerate(line):
+            grid.add_data_to_grid_at_x_y_z(letter, x, y)
+    # Part 2 Set corners
+    if part == 2:
+        set_corners(grid)
+
+
+def count_on_neighbors(key, grid):
+    ans = 0
+    neighbors = grid.get_neighbors_8way(grid.parse_pos_string(key))
+    for n in neighbors:
+        k = n.get_name()
+        if grid.grid[k] == '#':
+            ans += 1
+    return ans
+
+
+def count_on(grid):
+    count = 0
+    for v in grid.grid.values():
+        if v == '#':
+            count += 1
+    return count
+
+
+
+def update(grid, part=1):
+    t_grid = deepcopy(grid)
+    for key in grid.grid.keys():
+        status = grid.grid[key]
+        neighbors_on = count_on_neighbors(key, grid)
+        if status == '#':
+            if neighbors_on != 2 and neighbors_on != 3:
+                t_grid.grid[key] = '.'
+        elif status == '.':
+            if neighbors_on == 3:
+                t_grid.grid[key] = '#'
+    grid.grid = t_grid.grid
+    # Part 2 Set corners
+    if part == 2:
+        set_corners(grid)
+
+
+
+
+def main():
+    size = 100
+    grid = DictGrid(str, size)
+    init_grid(grid)
+    for _ in range(100):
+        update(grid)
+    # grid.display_grid_ascii()
+    print(f"Part 1 answer: {count_on(grid)}")
+
+    # Part 2
+
+    grid = DictGrid(str, size)
+    init_grid(grid, 2)
+
+    for _ in range(100):
+        update(grid, 2)
+    # grid.display_grid_ascii()
+    print(f"Part 2 answer: {count_on(grid)}")
+
+
+
+
+main()
+
+
---
+++ Classes/Grids.py
@@ -0,0 +1,133 @@
+from collections import defaultdict
+from Pos import Pos
+
+class DictGrid:
+    """
+    A grid where locations are only generated as they are explored
+    North or Up is Negative on the y-axis
+    Left or West is Negative on the x-axis
+    Grid dictionary is using with pos objects converted to string data for keys
+    Works with Pos objects.  Use pos.get_name() for the string rep
+    """
+    def __init__(self, datatype=list, size=None):
+        if size is not None:
+            self.bounded = True
+            self.size = size
+        else:
+            self.bounded = False
+        self.datatype = datatype
+        self.grid = defaultdict(datatype)
+        # (dx, dy) pairs for navigating
+        self.four_way_directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        self.eight_way_directions = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+
+    def add_data_to_grid_at_x_y_z(self, data, x, y, z=0):
+        pos = Pos(x, y, z)
+        self.add_data_to_grid_at_pos(data, pos)
+
+    def get_string_pos_key(self, pos):
+        return pos.get_name()
+
+    def add_data_to_grid_at_pos(self, data, pos):
+        if self.datatype == list:
+            self.grid[pos.get_name()].append(data)
+        elif self.datatype == set:
+            self.grid[pos.get_name()].add(data)
+        elif self.datatype == int:
+            self.grid[pos.get_name()] += data
+        elif self.datatype == str:
+            self.grid[pos.get_name()] = data
+        else:
+            raise NotImplemented(f"Datatype of {self.datatype} not implemented yet.")
+
+    def get_neighbors_4way(self, pos, exist=False):
+        """Returns an array of neighbors.
+        If the array is bounded, only in-bounds neighbors
+        If exist is True, only pre-existing neighbors"""
+        ans = []
+        for d in self.four_way_directions:
+            nx = pos[0] + d[0]
+            ny = pos[1] + d[1]
+            ans.append(Pos(nx, ny))
+
+        # Clean answers: Step 1.  If the grid is bounded, trip OOB
+        if self.bounded:
+            cleaned_ans = []
+            for p in ans:
+                if 0 <= p.x < self.size and 0 <= p.y < self.size:
+                    cleaned_ans.append(p)
+            ans = cleaned_ans
+
+        # Clean answers: Step 2. If the position is not already existing in the array, dump it
+        if exist:
+            cleaned_ans = []
+            for pos in ans:
+                if pos.get_name() in self.grid:
+                    cleaned_ans.append(pos)
+            ans = cleaned_ans
+        return ans
+
+    def get_neighbors_8way(self, pos, exist=False):
+        """Returns an array of neighbors.
+        If the array is bounded, only in-bounds neighbors
+        If exist is True, only pre-existing neighbors"""
+        ans = []
+        for d in self.eight_way_directions:
+            nx = pos[0] + d[0]
+            ny = pos[1] + d[1]
+            ans.append(Pos(nx, ny))
+
+        # Clean answers: Step 1.  If the grid is bounded, trip OOB
+        if self.bounded:
+            cleaned_ans = []
+            for p in ans:
+                if 0 <= p.x < self.size and 0 <= p.y < self.size:
+                    cleaned_ans.append(p)
+            ans = cleaned_ans
+
+        # Clean answers: Step 2. If the position is not already existing in the array, dump it
+        if exist:
+            cleaned_ans = []
+            for pos in ans:
+                if pos.get_name() in self.grid:
+                    cleaned_ans.append(pos)
+            ans = cleaned_ans
+        return ans
+
+    def parse_pos_string(self, s):
+        x, right = s.split('X | ')
+        x = int(x)
+        y = right[0:-1]
+        y = int(y)
+        return x, y
+
+    def get_graph_min_max(self):
+        min_x = None
+        min_y = None
+        max_x = None
+        max_y = None
+        for key in self.grid.keys():
+            x, y = self.parse_pos_string(key)
+            if min_x is None or x < min_x:
+                min_x = x
+            if max_x is None or x > max_x:
+                max_x = x
+            if min_y is None or y < min_y:
+                min_y = y
+            if max_y is None or y > max_y:
+                max_y = y
+        return min_x, max_x, min_y, max_y
+
+    def display_grid_ascii(self):
+        print('-' * 25)
+        min_x, max_x, min_y, max_y = self.get_graph_min_max()
+        for y in range(min_y, max_y + 1):
+            row = []
+            for x in range(min_x, max_x + 1):
+                t = Pos(x, y)
+                if t.get_name() in self.grid:
+                    row.append(self.grid[t.get_name()])
+                else:
+                    row.append('.')
+            print(row)
+        print('-' * 25)
