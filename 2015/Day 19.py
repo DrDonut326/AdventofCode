1d072a16221d54a54ee738e1b3ae0bebb9934e0f
Patrick Naven <pnaven@gmail.com>
2021-02-02 22:50:29 +0900

    Day 19
    Phew, this one was brutal.  I figured it was some kind of BFS problem, boy was I wrong.
    
    In the end had to look for some hints.  Turns out there's only one possible solution, so you can just collapse the heck out of it.

--- 2015/Day 19.py
+++ 2015/Day 19.py
@@ -1,63 +1,5 @@
-from collections import deque, defaultdict
+from collections import defaultdict
 from Utility import get_input
-
-# TODO: Figure out the explored set.  What do I need to add?
-# Redo.  Try to go from the finished product back down towards 'e'
-# Only explore paths
-
-
-class Graph:
-    """Find a path from start_string to end_string
-    Keeps track of the path and returns it.
-    Either depth first or breadth first search"""
-    def __init__(self, start_string, end_string, transform):
-        self.explored = set()
-        self.queue = deque()
-        self.start_string = start_string
-        self.end_string = end_string
-        self.initialize_queue()
-        # Transform: Should be a FUNCTION that returns a list of
-        # mutations from the given input string
-        self.transform = transform
-
-    def get_list_string(self, x):
-        assert type(x) == list
-        add_string = ''.join(x)
-        assert type(add_string) == str
-        return add_string
-
-    def initialize_queue(self):
-        """Sets up the starting condition of the search queue"""
-        self.queue.append([self.start_string])
-        self.explored.add(self.start_string)
-
-    def breadth_first_search(self, search_dict):
-        """Returns the shortest path from start to end strings"""
-        while len(self.queue) > 0:
-            # Item is a list (path)
-            item = self.queue.popleft()
-            assert type(item) == list
-            if item[-1] == self.end_string:
-                # Answer is found, return the whole path
-                return item
-
-            else:
-                next_list = []
-                # Need to mutate the string and add them back into the stack
-                mutations = self.transform(search_dict, item[-1])
-                for mut in mutations:
-                    if len(mut) <= len(self.end_string):
-                        t = item.copy()
-                        t.append(mut)
-                        if self.get_list_string(t) not in self.explored:
-                            next_list.append(t)
-                            print(t)
-                            print(self.end_string)
-                            quit()
-                            assert len(t[-1]) < self.end_string
-            if len(self.queue) == 0 and len(next_list) > 0:
-                for m in next_list:
-                    self.queue.append(m)
 
 
 def get_rules():
@@ -100,35 +42,6 @@ def mutate_forward(forwards, molecule):
     return mutations
 
 
-def mutate_backwards(backwards, molecule):
-    # Rules can be longer than one length!!!!!!!!!!
-
-    print(molecule)
-    quit()
-    found = set()  # Keep track of internal things found already
-
-    # Iterate through all the rules
-    # Substitute for each possible place
-    mutations = []
-    for f in backwards:
-        # Check to see if the letter is in the molecule
-        if f in molecule:
-            # Iterate through EACH RULE of that letter
-            for replacement in backwards[f]:
-                for i, letter in enumerate(molecule):
-                    # Iterate through the molecule looking for place to insert
-                    t = molecule[i: i + len(f)]
-                    if t == f:
-                        # If you find a match, create a new molecule
-
-                        new_molecule = molecule[0:i] + replacement + molecule[i+len(f):]
-
-                        # Check to see if the new molecule is already found
-                        if new_molecule not in found:
-                            mutations.append(new_molecule)
-                            found.add(new_molecule)
-
-
 def get_starting_molecule():
     with open("input.txt") as f:
         while True:
@@ -138,19 +51,30 @@ def get_starting_molecule():
         return f.readline().rstrip()
 
 
+def fully_collapse_molecule(molecule, backwards):
+    count = 0
+    while True:
+        if molecule == 'e':
+            return count
+        for key, repl in backwards.items():
+            repl = repl[0]
+            if key in molecule:
+                while key in molecule:
+                    molecule = molecule.replace(key, repl, 1)
+                    count += 1
+
+
 def main():
     # Dictionaries that govern what can be transformed
-    forwards, backwards = get_rules()   #TODO: Change for real input last line different
-    # starting_molecule = get_starting_molecule()
-    # next_mutations = mutate_forward(forwards, starting_molecule)
-    # print(f"Part 1 Answer: {len(next_mutations)}")
+    forwards, backwards = get_rules()
+    starting_molecule = get_starting_molecule()
+    next_mutations = mutate_forward(forwards, starting_molecule)
+    print(f"Part 1 Answer: {len(next_mutations)}")
 
     # Part 2
-    starting_molecule = 'e'
-    goal_molecule = get_starting_molecule()
-    graph = Graph(starting_molecule, goal_molecule, mutate_forward)
-    ans = graph.breadth_first_search(forwards)
-    print(ans)
+    starting_molecule = get_starting_molecule()
+    count = fully_collapse_molecule(starting_molecule, backwards)
+    print(f"Part 2 Answer: {count}")
 
 
 main()
--- Utility/Utility.py
+++ Utility/Utility.py
@@ -1,6 +1,7 @@
 # Utility methods for quickly import data
 from functools import reduce
 
+
 def get_input(line_or_char, text_location='input.txt', do_split=False, split_key='', early_stop=False, stop_char=''):
     """Gets the input, and spits it into a list
     By default goes line-by-line
@@ -24,5 +25,26 @@ def get_input(line_or_char, text_location='input.txt', do_split=False, split_key
 
 
 def factors(n):
-    return set(reduce(list.__add__,
-                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+    return set(reduce(list.__add__, ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def get_indices_of_substring(full_string, sub_string):
+    """Returns a list of indicies of locations of the
+    sub string in the full string"""
+    assert sub_string in full_string
+    ans = []
+    i = 0
+    while True:
+        try:
+            result = full_string.index(sub_string, i)
+        except ValueError:
+            return ans
+        ans.append(result)
+        i = result + 1
+
+
+def get_new_string_with_substring_at_index(full_string, target, sub_string, i):
+    """Used in combination with 'get_indices_of_substring'
+    To replace all instances of a string with a substring"""
+    assert type(full_string) == str and type(sub_string) == str and type(i) == int
+    assert 0 <= i < len(full_string)
+    return full_string[0: i] + full_string[i:].replace(target, sub_string, 1)
