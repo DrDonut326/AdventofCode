74f9bd5edbaf5ea13dc6ae4ff9cc0ce045c5bab9
Patrick Naven <pnaven@gmail.com>
2022-06-16 16:43:42 -0600

    2017 / Day 1
    Modular math to avoid looping around.

---
+++ 2021/Day 16.py
@@ -0,0 +1,45 @@
+from Utility import get_input
+
+
+def bin_convert(x):
+    """Converts a single character hex string into a 4 digit binary string with leading zeros."""
+    assert type(x) == str
+
+    # Convert to decimal
+    decimal = int(x, 16)
+
+    # Convert to binary
+    bin_d = bin(decimal)
+
+    ans = bin_d[2:].zfill(4)
+
+    return ans
+
+
+def unpack_input(hex_string):
+    ans = ''
+    for num in hex_string:
+        ans += bin_convert(num)
+    return ans
+
+
+def get_packet_version(x):
+    p = x[0:3]
+    ans = int(p, 2)
+    return ans
+
+
+def get_packet_type(x):
+    p = x[3:6]
+    ans = int(p, 2)
+    return ans
+
+
+def main():
+    hex_string = get_input('line')[0]
+    bin_string = unpack_input(hex_string)
+
+    get_packet_type(bin_string)
+
+
+main()
