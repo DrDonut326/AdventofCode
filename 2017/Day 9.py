3bc7e11a3b2dc2ba7399c95605b003fb7ad29f59
Patrick Naven <pnaven@gmail.com>
2022-06-18 22:47:05 -0600

    2017 / Day 9
    Part 1 seemed recurisve-ish, but a simple step-through approach worked much better.
    
    Part 2 was trivial after my data cleaning approach.

---
+++ 2017/Day 9.py
@@ -0,0 +1,80 @@
+from Utility import get_input
+
+
+# Use ? for bad chars
+
+
+def get_char_set(data):
+    ans = set()
+    for element in data:
+        ans.add(element)
+    return ans
+
+
+def clean_chars(data):
+    # Mark for termination
+    for i, letter in enumerate(data):
+        if letter == '!':
+            data[i] = '?'
+            data[i + 1] = '?'
+
+    # Terminate
+    while '?' in data:
+        data.remove('?')
+
+
+def standardize_garbage(data):
+    """"Makes all garbage data '?'"""
+    change = False
+    for i, letter in enumerate(data):
+        # If change is currently True, change things
+        if change:
+            # If We reach a closing string, turn off change
+            if letter == '>':
+                change = False
+            else:
+                data[i] = '?'                # TODO Count garbage later?
+        else:
+            # Change is False, check for start
+            if letter == '<':
+                change = True
+
+
+def display_puzzle(data):
+    print(''.join(data))
+
+
+def count_groups(data):
+    # Add to total every time a group is closed
+    level = 0
+    total = 0
+    for letter in data:
+        if letter == '{':
+            level += 1
+
+        elif letter == '}':
+            total += level
+            level -= 1
+    return total
+
+
+
+
+
+def main():
+    puzzle = get_input('char')
+
+    # Clean up data
+    clean_chars(puzzle)
+    standardize_garbage(puzzle)
+
+    # Part 1
+    score = count_groups(puzzle)
+    print(score)
+
+    # Part 2
+    print(puzzle.count('?'))
+
+
+
+main()
