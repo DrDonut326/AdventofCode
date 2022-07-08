be12d6b7c4f00d8ecbc5f208c1f4c47a9721e86e
Patrick Naven <pnaven@gmail.com>
2022-07-07 22:24:50 -0600

    This result should get you very close to the answer.

--- 2018/Day10.py
+++ 2018/Day10.py
@@ -1,8 +1,9 @@
 from Pos import Pos
 from Grids import DictGrid
+from collections import defaultdict
 
 
-def get_input(speed_multiplier):
+def get_input():
     lights = []
     with open("input.txt") as f:
         for line in f:
@@ -12,8 +13,6 @@ def get_input(speed_multiplier):
             line = line.replace('velocity=<', ' ')
             line = line.replace(', ', ' ')
             x, y, dx, dy = [int(z) for z in line.split()]
-            dx *= speed_multiplier
-            dy *= speed_multiplier
             pos = Pos(x, y, dx=dx, dy=dy)
             lights.append(pos)
     return lights
@@ -32,18 +31,42 @@ def show_lights(lights):
     grid.display_grid_ascii(data_override='*')
 
 
-
-def update_lights(lights: list):
-    light: Pos
+def get_total_distance(lights):
+    total = 0
     for light in lights:
-        print(light)
+        total += abs(light.x)
+        total += abs(light.y)
+    return total
 
-        quit()
+
+def get_same_y_count(lights):
+    sames = defaultdict(int)
+    for light in lights:
+        sames[light.y] += 1
+    # Get max same score
+    total = max(sames.values())
+    return total
+
+
+def update_lights(lights):
+    for light in lights:
+        light.x += light.dx
+        light.y += light.dy
+
+
+
+
 
 def main():
-    lights = get_input(1)
-    update_lights(lights)
-    show_lights(lights)
+    lights = get_input()
+    same = 0
+    seconds = 1
+    while same < 22:
+        seconds += 1
+        update_lights(lights)
+        same = get_same_y_count(lights)
+
+    print(seconds)
 
 
 main()
