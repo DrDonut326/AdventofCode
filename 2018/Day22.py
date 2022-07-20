68e800a7c9632d3cd7ccc0d3ba10fe9e1e346dfc
Patrick Naven <pnaven@gmail.com>
2022-07-20 13:36:50 -0600

    2018 / Day 22
    Implemented a Dijkstra search, getting everything prepped to test for multidimensional cave searching.

---
+++ 2018/Day22.py
@@ -0,0 +1,109 @@
+from functools import lru_cache
+from Utility.Pathfinding import dijkstra_dictgrid
+from Classes.Grids import DictGrid
+
+class Cave:
+    risk_dict = {'rocky': 0, 'wet': 1, 'narrow': 2}
+    risk_symbols = {'rocky': '.', 'wet': '=', 'narrow': '|'}
+
+    def __init__(self, pos, target_pos, depth):
+        self.pos = pos
+        self.target_pos = target_pos
+        self.depth = depth
+
+    @lru_cache(maxsize=None)
+    def get_geologic_index(self, pos):
+        x, y = pos
+
+        if pos == (0, 0):
+            return 0
+
+        if pos == self.target_pos:
+            return 0
+
+        if y == 0:
+            return x * 16807
+
+        if x == 0:
+            return y * 48271
+
+        return self.get_erosion_level((x-1, y)) * self.get_erosion_level((x, y-1))
+
+    @lru_cache(maxsize=None)
+    def get_erosion_level(self, pos):
+        return (self.get_geologic_index(pos) + self.depth) % 20183
+
+    @lru_cache(maxsize=None)
+    def get_risk_type(self, pos):
+        fine_erosion = self.get_erosion_level(pos) % 3
+        if fine_erosion == 0:
+            return 'rocky'
+        elif fine_erosion == 1:
+            return 'wet'
+        elif fine_erosion == 2:
+            return 'narrow'
+        else:
+            raise EnvironmentError('bad erosion type')
+
+    def get_total_risk(self):
+        total_risk = 0
+        for y in range(0, self.target_pos[1] + 1):
+            for x in range(0, self.target_pos[0] + 1):
+                pos = (x, y)
+                total_risk += self.risk_dict[self.get_risk_type(pos)]
+        return total_risk
+
+    def get_allowable_equipment(self, cave_type):
+        if cave_type == 'rocky':
+            return [1, 2]
+        if cave_type == 'wet':
+            return [0, 2]
+        if cave_type == 'narrow':
+            return [0, 1]
+
+
+def find_neighbors_cave(grid: DictGrid, pos: tuple):
+    # Get neighbros
+    neighbors = grid.get_neighbors_4way(pos[0], pos[1])
+
+    # Prune < 0
+    neighbors = [n for n in neighbors if n[0] >= 0 and n[1] >=0]
+
+    # Locate the type of this current position???
+    # todo: is this a tuple with cost or what?
+
+
+def part_1(target, depth):
+    cave = Cave((0, 0), target, depth)
+    print(cave.get_total_risk())
+    return cave
+
+
+def add_locations_to_grid(grid: DictGrid, cave: Cave, pos: tuple):
+    """Given a location in the cave, adds appropriate z level dimensions to it."""
+    # 0 = neither equipment
+    # 1 = torch
+    # 2 = climbing gear
+
+    # Get the type of this location
+    cave_type = cave.get_risk_type(pos)
+
+    # Get allowable equipment numbers
+    equip_nums = cave.get_allowable_equipment(cave_type)
+
+    # Add each equipment number as a z value to the tuple
+    for equip in equip_nums:
+        cave_pos = (pos[0], pos[1], equip)
+
+        # Add it to the grid as a location
+        grid.add_data_to_grid_at_pos(cave_pos, cave.risk_symbols[cave_type])
+
+def main():
+    depth = 10647
+    target_pos = (7, 770)
+    cave = part_1(target_pos, depth)  # Part 1 answer
+
+    grid = DictGrid(str)
+    grid
+
+main()
--- Utility/Pathfinding.py
+++ Utility/Pathfinding.py
@@ -1,7 +1,22 @@
+import heapq
 from collections import deque
-
+from random import randint
 from Functions import Pos
 from Grids import DictGrid, ArrayGrid
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self) -> bool:
+        return not self.elements
+
+    def put(self, pos, priority):
+        heapq.heappush(self.elements, (priority, pos))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
 
 
 class Node:
@@ -31,23 +46,6 @@ class Node:
         return self.cost != other.cost
 
 
-def get_test_input():
-    grid = DictGrid(None, {'#', 'S', 'F', 'G', 'E'}, bounded=True)
-    start = None
-    finish = None
-    with open('test_input.txt') as f:
-        for y, line in enumerate(f):
-            line = line.rstrip()
-            for x, element in enumerate(line):
-                pos = Pos(x, y)
-                grid.grid[pos] = element
-                if element == 'S':
-                    start = pos
-                if element == 'F':
-                    finish = pos
-    return grid, start, finish
-
-
 def bfs(grid: ArrayGrid, x: int, y: int):
     """Creates a bfs cost grid using a simple 2D array."""
     q = deque()
@@ -70,7 +68,6 @@ def bfs(grid: ArrayGrid, x: int, y: int):
     return distance
 
 
-
 def bfs_dictgrid(grid: DictGrid, start: tuple):
     """Creates a cost grid that radiates out from the starting position.  Searches all possible cells."""
     q = deque()
@@ -89,6 +86,46 @@ def bfs_dictgrid(grid: DictGrid, start: tuple):
                     distance[next_pos] = 1 + distance[current_pos]
 
     return distance
+
+
+def dijkstra_dictgrid(grid: DictGrid, start: tuple, finish: tuple, neighbor_func):
+    """Searches a dict grid for the shortest path between start and finish.
+    User must pass in a function that tells Dijkstra how to find neighbors and their costs."""
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = dict()
+    cost_so_far = dict()
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == finish:
+            break
+
+        # Get the pos and cost of that neighbors of this position
+        for neighbor, neighbor_cost in neighbor_func(grid, current):
+            # Get the cost of going from current to neighbor
+            new_cost = cost_so_far[current] + neighbor_cost
+            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                cost_so_far[neighbor] = new_cost
+                priority = new_cost
+                frontier.put(neighbor, priority)
+                came_from[neighbor] = current
+
+    return came_from, cost_so_far
+
+
+def rebuild_path(came_from, start, finish):
+    current = finish
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start)
+    path.reverse()
+    return path
 
 
 def display_cost_grid(grid, cost_grid):
@@ -115,12 +152,50 @@ def display_cost_grid(grid, cost_grid):
     print('-' * 25)
 
 
+def get_random_grid(size):
+    # Create a size x size grid of random numbers
+    grid = []
+    for a in range(size):
+        row = []
+        for b in range(size):
+            row.append(randint(0, 9))
+        grid.append(row)
+    return grid
+
+
+def get_random_dict_grid(size):
+    grid = DictGrid(int)
+    array = get_random_grid(size)
+    for y, row in enumerate(array):
+        for x, element in enumerate(row):
+            pos = (x, y)
+            grid.add_data_to_grid_at_pos(pos, element)
+    return grid
+
+
+def neighbor_function_use_grid_data(grid: DictGrid, pos):
+    """Returns the neighbors of this position and also the cost from pos to neighbor."""
+    neighbors = grid.get_neighbors_4way(pos[0], pos[1], exist_prune=True)
+
+    ans = []
+    for n in neighbors:
+        cost = grid.get_value_from_pos_object(n)
+        ans.append((n, cost))
+    return ans
+
 def main():
-    grid, start, finish = get_test_input()
-    cost_grid = bfs_dictgrid(grid, start, finish)
-    grid.display_grid_with_highlighted_positions_all_same([start, finish], (200, 235, 50))
+    size = 2
+    grid = get_random_dict_grid(size)
+    grid.display_grid_ascii()
+    start = (0, 0)
+    finish = (size - 1, size - 1)
 
+    came_from, cost_so_far = dijkstra_dictgrid(grid, start, finish, neighbor_function_use_grid_data)
 
+    path = rebuild_path(came_from, start, finish)
+
+    grid.display_grid_with_highlighted_positions_all_same(path, (0, 255, 0))
+    print(cost_so_far[finish])
 
 
 if __name__ == '__main__':
